3774c9b5d2c2f09a3f8645056cb9b0479ccbc62e
冠登 廖 <t8599008@ntut.org.tw>
2021-04-26 18:07:18 +0800

    加入專案檔案。

---
+++ WebApp_F/ShowTime.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp_F
+{
+    public partial class ShowTime : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e){
+            Person o_Per1 = new Person();
+
+        }
+
+        protected void btn_ShowTime_Click(object sender, EventArgs e)
+        {
+            int i_Thx;
+            Response.Write("目前的時間： "+ DateTime.Now.ToString());
+        }
+    }
+}
---
+++ WebApp_F/ShowTime.aspx.designer.cs
@@ -0,0 +1,49 @@
+﻿//------------------------------------------------------------------------------
+// <自動產生的>
+//     這段程式碼是由工具產生的。
+//
+//     變更這個檔案可能會導致不正確的行為，而且如果已重新產生
+//     程式碼，則會遺失變更。
+// </自動產生的>
+//------------------------------------------------------------------------------
+
+namespace WebApp_F
+{
+
+
+    public partial class ShowTime
+    {
+
+        /// <summary>
+        /// form1 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// btn_ShowTime 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_ShowTime;
+    }
+
+    public class Person{
+        public string s_Name;
+        public string s_Id;
+
+        public Person() {
+            s_Name = "";
+            s_Id = "";
+        }
+        public void mt_SetName(string s_Name) {
+            this.s_Name = s_Name;
+        }
+    }
+
+}
