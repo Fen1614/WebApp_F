63ecf8f207bfd74ded7bfdc170e62d9211e86934
altoliaw <altoliaw@hotmail.com>
2021-05-17 15:20:14 +0800

    8-2

---
+++ WebApp_F/ch8-2_1.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace WebApp_F {
+    public partial class ch8_2_1 : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            string s_ConnS = "";
+            s_ConnS += "server=localhost;";
+            s_ConnS += "database=Test;";
+            s_ConnS += "user id=sa;";
+            s_ConnS += "password=a74123698;";
+
+            SqlConnection o_Conn = new SqlConnection(s_ConnS);
+            o_Conn.Open();
+            //各種資料庫操作
+            Response.Write("Connecction");
+            o_Conn.Close();
+        }
+    }
+}
