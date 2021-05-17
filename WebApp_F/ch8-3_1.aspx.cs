0327492315155ec052a26978954d8f8026901747
altoliaw <altoliaw@hotmail.com>
2021-05-17 18:53:15 +0800

    Chapter 8

--- WebApp_F/ch8-3_1.aspx.cs
+++ WebApp_F/ch8-3_1.aspx.cs
@@ -2,7 +2,7 @@
 using System.Data.SqlClient;
 
 namespace WebApp_F {
-    public partial class ch8_2_1 : System.Web.UI.Page {
+    public partial class ch8_3_1 : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
             string s_ConnS = "";
 
@@ -11,17 +11,25 @@ namespace WebApp_F {
                 "Integrated Security=True;" +
                 "Connect Timeout=30;Encrypt=False;" +
                 "TrustServerCertificate=False;" +
-                "ApplicationIntent=ReadWrite;MultiSubnetFailover=False;"+
+                "ApplicationIntent=ReadWrite;MultiSubnetFailover=False;" +
                 "User ID = sa; Password = 12345";
 
             try {
                 SqlConnection o_Conn = new SqlConnection(s_ConnS);
+                
+                SqlCommand o_Com = new SqlCommand("Select * from Users", o_Conn);
                 o_Conn.Open();
-                //各種資料庫操作
-                Response.Write("Connected");
+                SqlDataReader o_R =  o_Com.ExecuteReader();
+                for (; o_R.Read(); ) {
+                    for (int i_Col=0; i_Col< o_R.FieldCount; i_Col++) {
+                        Response.Write("&nbsp;&nbsp;"+ o_R[i_Col].ToString());
+                    }
+                    Response.Write("<br />");
+                }
                 o_Conn.Close();
             }
-            catch (Exception o_Exc) {
+            catch (Exception o_Exc)
+            {
                 Response.Write(o_Exc.ToString());
             }
         }
