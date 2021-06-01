8472f8c64adc80aab106189cde920f83eeb6d452
altoliaw <altoliaw@hotmail.com>
2021-06-01 12:49:02 +0800

    WebService

---
+++ WebApp_F/ch11-3_2.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp_F {
+    public partial class ch11_3_2 : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            ServiceReference1.ch11_3_1SoapClient o_WS =
+                new ServiceReference1.ch11_3_1SoapClient();
+            
+            Response.Write(o_WS.HelloWorld());
+        }   
+    }
+}
